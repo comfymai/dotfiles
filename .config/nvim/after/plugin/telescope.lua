b5a79dfec55913e913c43870864a3a6ecbe8b982
Maya <mkzk.mail@gmail.com>
2022-12-28 20:29:22 -0300

    chore: neovim ignore rust targets

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,7 +3,7 @@ local builtin = require("telescope.builtin")
 
 telescope.setup {
     defaults = {
-        file_ignore_patterns = { "node_modules", "yarn.lock" }
+        file_ignore_patterns = { "node_modules", "yarn.lock", "target/debug" }
     },
     extensions = {
         file_browser = {
