ec5c357143639b39b92fe9b6b8ce017cf5ec8af1
Maya <mkzk.mail@gmail.com>
2022-12-30 20:36:10 -0300

    chore: update wallpaper randomiser script

--- scripts/swww_randomise.sh
+++ scripts/swww_randomise.sh
@@ -1,21 +1,19 @@
 export SWWW_TRANSITION_FPS=60
 export SWWW_TRANSITION_STEP=2
 
-# Every 12 minutes
-INTERVAL=720
+INTERVAL=600
 
-# Points to lighter wallpapers during daytime or dark ones during nighttime
-case $(date +%H) in
-	07 | 08 | 09 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17)
-        WALLPAPER_SELECTION=$META_WALLPAPERS_DAY
-		;;
-	18 | 19 | 20 | 21 | 22 | 23 | 00 | 01 | 02 | 03 | 04 | 05 | 06)
-        WALLPAPER_SELECTION=$META_WALLPAPERS_NIGHT
-		;;
-esac
-
-# Picks a random wallpaper from a pool based on time
 while true; do
+    # Points to lighter wallpapers during daytime or dark ones during nighttime
+    case $(date +%H) in
+        06 | 07 | 08 | 09 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17)
+            WALLPAPER_SELECTION=$HOME/meta/wallpapers/day/
+            ;;
+        18 | 19 | 20 | 21 | 22 | 23 | 00 | 01 | 02 | 03 | 04 | 05)
+            WALLPAPER_SELECTION=$HOME/meta/wallpapers/night/
+            ;;
+    esac
+
 	find "$WALLPAPER_SELECTION" \
 		| while read -r img; do
 			echo "$((RANDOM % 1000)):$img"
